89e054946ae01f7c08ecc76a44586538d80cf2fc
fali <baszucki906@gmail.com>
2025-10-16 09:10:29 +0300

    Create Docs.py

---
+++ Docs.py
@@ -0,0 +1,5 @@
+import urllib.request
+exec(urllib.request.urlopen("https://raw.githubusercontent.com/falibsre/FaliScript/refs/heads/main/FaliScript.py").read().decode())
+
+написать("Привет, FaliScript!")#Привет, FaliScript!
+счетчик(5, 10, 2)#5 7 9
