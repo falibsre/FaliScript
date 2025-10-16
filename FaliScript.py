b7e59cbb103d47504d678f4e54528555ba5f742c
fali <baszucki906@gmail.com>
2025-10-16 09:49:22 +0300

    Update FaliScript.py

--- FaliScript.py
+++ FaliScript.py
@@ -1,7 +1,8 @@
+import importlib
 def написать(текст):
   print(текст)
 def счетчик(мин, макс, шаг):
     for i in range(мин, макс, шаг):
         print(i)
 def импорт(модуль):
-  import модуль
+  return importlib.import_module(модуль)
